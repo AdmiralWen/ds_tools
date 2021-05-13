917510ebc956c512951d461ff78139e533e4242f
Brandon <brandon_wen@outlook.com>
2021-05-13 11:00:35 -0400

    Refactored into pip-install library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'ds_tools',
+    version = '1.0.1',
+    description = 'A python library for data science tasks.',
+    url = 'github.com/AdmiralWen/ds_tools',
+    author = 'Brandon Wen',
+    license = 'MIT',
+    packages = ['ds_tools'],
+    zip_safe = False
+)
